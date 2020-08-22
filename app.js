ab37a4828674e04587f2b6f6b4df87f439f04e47
Ajay Raj <ajay@vizexperts.com>
2020-08-22 15:01:15 +0530

    clear the input fields after adding the item

--- app.js
+++ app.js
@@ -94,6 +94,20 @@ var UIController = (function () {
             //add list in the UI
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMStrings.inputDescription + ', ' +DOMStrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            //loop over the whole fields array and set the empty value
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+        },
         getDomStrings: function () {
             return DOMStrings;
         }
@@ -129,6 +143,9 @@ var appController = (function (budgetCtrl, UICtrl) {
 
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
+
+        // 4. Clear the input fields
+        UICtrl.clearFields();
 
         // 4. Calculate the budget
         // 5. Display the budget on the UI
