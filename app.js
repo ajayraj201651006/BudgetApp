35b49e602ecd825920550be634eb1a206a95223a
Ajay Raj <ajay@vizexperts.com>
2020-08-22 13:23:13 +0530

    adding a new item into the budget controller

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 
 //BUDGET CONTROLLER
 var budgetController = (function () {
-    var Expenses = function(id, description, val) {
+    var Expenses = function (id, description, val) {
         this.id = id;
         this.description = description;
         this.value = val;
     };
 
-    var Incomes = function(id, description, val) {
+    var Incomes = function (id, description, val) {
         this.id = id;
         this.description = description;
         this.value = val;
@@ -23,6 +23,35 @@ var budgetController = (function () {
             inc: 0
         }
     };
+
+    return {
+        addItem: function (type, des, value) {
+            var newItem, ID;
+
+            //Create unique ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            //craete newItem based on 'exp' or 'inc'
+            if (type === 'exp') {
+                newItem = new Expenses(ID, des, value);
+            } else if (type === 'inc') {
+                newItem = new Incomes(ID, des, value);
+            }
+
+            //push the new item into our data structure
+            data.allItems[type].push(newItem);
+
+            //return new item
+            return newItem;
+        },
+        testing: function () {
+            return data;
+        }
+    }
 })();
 
 //UI CONTROLLER
@@ -68,10 +97,14 @@ var appController = (function (budgetCtrl, UICtrl) {
 
     var ctrlEvent = function () {
 
+        var input, newItem;
+
         // 1. Get Input Values
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
 
         // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
         // 3. Add the item to the UI
         // 4. Calculate the budget
         // 5. Display the budget on the UI
